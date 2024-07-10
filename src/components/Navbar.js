af8bca58f7059b01c98aa838737c3bd9b69c9204
Kyler Besher <kyler@chekhub.com>
2024-07-10 12:23:44 -0500

    working, nav not implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,15 @@
 import React, { useState } from "react";
-import { Link, graphql, useStaticQuery } from "gatsby";
-import { Facebook as FacebookIcon } from '@mui/icons-material';
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+// import { Facebook } from '@mui/icons';
+// import { FacebookIcon } from '@mui/icons-material';
+import { Facebook as FacebookIcon, Settings as SettingsIcon, AccountCircle as AccountCircleIcon } from '@mui/icons-material';
+
+
 import logo from "../img/diehl_logo.png";
-
 const height = '80px';
-
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
-
-  const data = useStaticQuery(graphql`
-    query PagesQuery {
-      allMarkdownRemark(
-        sort: { order: ASC, fields: [frontmatter___title] }
-        filter: { frontmatter: { templateKey: { eq: "general-page" } } }
-      ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  const pages = data.allMarkdownRemark.edges;
 
   return (
     <>
@@ -42,10 +22,11 @@ const Navbar = () => {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Diehl Construction" style={{ width: "88px" }} />
+              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
             </Link>
+            {/* Hamburger menu */}
             <button
-              className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+              className={`navbar-burger burger ${isActive && "is-active"}`}
               aria-expanded={isActive}
               onClick={() => setIsActive(!isActive)}
             >
@@ -56,15 +37,38 @@ const Navbar = () => {
           </div>
           <ul
             id="navMenu"
-            className={`navbar-start has-text-centered navbar-menu ${isActive ? "is-active" : ""}`}
+            className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"
+              }`}
           >
-            {pages.map(({ node: page }) => (
-              <li key={page.id} className="navbar-item" style={{ padding: "0px" }}>
-                <Link className="navbar-item" to={page.fields.slug}>
-                  {page.frontmatter.title}
-                </Link>
-              </li>
-            ))}
+            {/* TODO: inline override of padding is a result of refactoring
+                to a ul for accessibilty purposes, would like to see a css
+                re-write that makes this unneccesary.
+             */}
+            <li className="navbar-item" style={{ padding: "0px" }}>
+              <Link className="navbar-item" to="/foundation-repair">
+                Basement & Foundation Repair
+              </Link>
+            </li>
+            <li className="navbar-item" style={{ padding: "0px" }}>
+              <Link className="navbar-item" to="/products">
+                Waterproofing
+              </Link>
+            </li>
+            <li className="navbar-item" style={{ padding: "0px" }}>
+              <Link className="navbar-item" to="/blog">
+                Dirt Work
+              </Link>
+            </li>
+            <li className="navbar-item" style={{ padding: "0px" }}>
+              <Link className="navbar-item" to="/contact">
+                Egress Window Installation
+              </Link>
+            </li>
+            <li className="navbar-item" style={{ padding: "0px" }}>
+              <Link className="navbar-item" to="/contact/examples">
+                Contact
+              </Link>
+            </li>
             <li className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
@@ -80,7 +84,7 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
-      <div style={{ height: height }}></div>
+      <div style={{ height: height }}></div >
     </>
   );
 };
