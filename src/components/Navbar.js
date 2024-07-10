1fd8b841deea84c80a53ee040ce9381cf6b4e40e
Kyler Besher <kyler@chekhub.com>
2024-07-10 11:38:08 -0500

    broken

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-// import { Facebook } from '@mui/icons';
-// import { FacebookIcon } from '@mui/icons-material';
-import { Facebook as FacebookIcon, Settings as SettingsIcon, AccountCircle as AccountCircleIcon } from '@mui/icons-material';
-
-
+import { Link, StaticQuery, graphql } from "gatsby";
+import { Facebook as FacebookIcon } from '@mui/icons-material';
 import logo from "../img/diehl_logo.png";
+
 const height = '80px';
-const Navbar = () => {
+
+const NavbarTemplate = ({ data }) => {
   const [isActive, setIsActive] = useState(false);
+  const pages = data?.allMarkdownRemark?.edges || [];
 
   return (
     <>
@@ -22,11 +20,10 @@ const Navbar = () => {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Diehl Construction" style={{ width: "88px" }} />
             </Link>
-            {/* Hamburger menu */}
             <button
-              className={`navbar-burger burger ${isActive && "is-active"}`}
+              className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
               aria-expanded={isActive}
               onClick={() => setIsActive(!isActive)}
             >
@@ -37,38 +34,18 @@ const Navbar = () => {
           </div>
           <ul
             id="navMenu"
-            className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"
-              }`}
+            className={`navbar-start has-text-centered navbar-menu ${isActive ? "is-active" : ""}`}
           >
-            {/* TODO: inline override of padding is a result of refactoring
-                to a ul for accessibilty purposes, would like to see a css
-                re-write that makes this unneccesary.
-             */}
-            <li className="navbar-item" style={{ padding: "0px" }}>
-              <Link className="navbar-item" to="/foundation-repair">
-                Basement & Foundation Repair
-              </Link>
-            </li>
-            <li className="navbar-item" style={{ padding: "0px" }}>
-              <Link className="navbar-item" to="/products">
-                Waterproofing
-              </Link>
-            </li>
-            <li className="navbar-item" style={{ padding: "0px" }}>
-              <Link className="navbar-item" to="/blog">
-                Dirt Work
-              </Link>
-            </li>
-            <li className="navbar-item" style={{ padding: "0px" }}>
-              <Link className="navbar-item" to="/contact">
-                Egress Window Installation
-              </Link>
-            </li>
-            <li className="navbar-item" style={{ padding: "0px" }}>
-              <Link className="navbar-item" to="/contact/examples">
-                Contact
-              </Link>
-            </li>
+            {pages.map(({ node: page }) => (
+              <li key={page.id} className="navbar-item" style={{ padding: "0px" }}>
+                <Link className="navbar-item" to={page.fields.slug}>
+                  {page.frontmatter.title}
+                </Link>
+              </li>
+            ))}
+            {/* <Link className="navbar-item" to="/contact">
+                  Contact
+                </Link> */}
             <li className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
@@ -84,9 +61,35 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
-      <div style={{ height: height }}></div >
+      <div style={{ height: height }}></div>
     </>
   );
 };
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query PagesQuery {
+          allMarkdownRemark(
+            sort: { order: ASC, fields: [frontmatter___title] }
+            filter: { frontmatter: { templateKey: { eq: "general-page" } } }
+          ) {
+            edges {
+              node {
+                id
+                frontmatter {
+                  title
+                }
+                fields {
+                  slug
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => <NavbarTemplate data={data} />}
+    />
+  );
+}
