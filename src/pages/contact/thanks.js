43686a6b78450a37b7a571d537b9d0df61f112ce
Kyler Besher <kyler@chekhub.com>
2024-07-11 15:08:25 -0500

    update og and favicon

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,30 +7,13 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content has-text-centered">
-          <h1 className="title is-1 has-text-primary">Thank You!</h1>
+          <h1 style={{marginBottom:"3rem"}} className="title is-1 has-text-primary">Thank You!</h1>
           <p className="subtitle is-4 has-text-grey-dark">
-            We appreciate you getting in touch with us. One of our team members will get back to you shortly.
+            We appreciate you getting in touch with us. 
           </p>
-          <div className="box">
-            <p className="is-size-5 has-text-grey">
-              If your inquiry is urgent, please use the telephone number listed below to talk to one of our staff members. Otherwise, we will reply by email as soon as possible.
-            </p>
-            <br />
-            <p className="is-size-5 has-text-weight-bold">Contact Information</p>
-            <p className="is-size-5">
-              Phone: <a href="tel:+15551234567">(555) 123-4567</a>
-            </p>
-            <p className="is-size-5">
-              Email: <a href="mailto:info@diehlconstruction.com">info@diehlconstruction.com</a>
-            </p>
-          </div>
-          <div className="section">
-            <p className="is-size-5">
-              In the meantime, feel free to browse through our 
-              <a href="/services"> services</a> or visit our 
-              <a href="/blog"> blog</a> for the latest updates and insights.
-            </p>
-          </div>
+          <p className="subtitle is-4 has-text-grey-dark">
+            One of our team members will get back to you shortly.
+          </p>
         </div>
       </div>
     </section>
